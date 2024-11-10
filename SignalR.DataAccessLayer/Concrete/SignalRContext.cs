7c8e84f1611623115edfa9591b9c935a6b08b3ab
Gulsah <gulsharslan@gmail.com>
2024-11-10 15:57:03 +0300

    ..

--- SignalR.DataAccessLayer/Concrete/SignalRContext.cs
+++ SignalR.DataAccessLayer/Concrete/SignalRContext.cs
@@ -29,5 +29,6 @@ namespace SignalR.DataAccessLayer.Concrete
         public DbSet<MoneyCase> MoneyCases { get; set; }
         public DbSet<MenuTable> MenuTables { get; set; }
         public DbSet<Basket> Baskets { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
     }
 }
