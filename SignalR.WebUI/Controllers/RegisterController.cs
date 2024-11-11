92bc7452b0e6ae915e9d737882edc20b50964c8d
Gulsah <gulsharslan@gmail.com>
2024-11-11 16:49:07 +0300

    ..

---
+++ SignalR.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.EntityLayer.Entities;
+using SignalR.WebUI.Dtos.IdentityDtos;
+
+namespace SignalR.WebUI.Controllers
+{
+    [AllowAnonymous]
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(RegisterDto registerDto)
+        {
+            var appUser = new AppUser()
+            {
+                Email = registerDto.Email,
+                Name = registerDto.Name,
+                Surname = registerDto.Surname,
+                UserName = registerDto.Username,
+            };
+            var result = await _userManager.CreateAsync(appUser, registerDto.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+    }
+}
