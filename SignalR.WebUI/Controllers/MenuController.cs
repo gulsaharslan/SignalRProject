a216327b3f67d6f0d12eaa31dab1986ba5965ab1
Gulsah <gulsharslan@gmail.com>
2024-11-18 16:47:31 +0300

    ..

--- SignalR.WebUI/Controllers/MenuController.cs
+++ SignalR.WebUI/Controllers/MenuController.cs
@@ -18,7 +18,7 @@ namespace SignalR.WebUI.Controllers
         public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7018/api/Products");
+            var responseMessage = await client.GetAsync("https://localhost:7018/api/Products/ProductListWithCategory");
             var jsonData = await responseMessage.Content.ReadAsStringAsync();
             var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
             return View(values);
--- SignalR.WebUI/ViewComponents/MenuComponents/_MenuCategoryFilterComponentPartial.cs
+++ SignalR.WebUI/ViewComponents/MenuComponents/_MenuCategoryFilterComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalR.WebUI.Dtos.CategoryDtos;
 
 namespace SignalR.WebUI.ViewComponents.MenuComponents
 {
     public class _MenuCategoryFilterComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _MenuCategoryFilterComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7018/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
